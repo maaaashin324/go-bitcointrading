430f52489f9b12b344f32e6d87dfff487b6c7c32
maaaashin324 <masataka.shintoku@gmail.com>
2020-07-11 10:48:31 +0900

    Added config.go

---
+++ config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"gopkg.in/ini.v1"
+)
+
+// List can be used to get any environment variable
+type List struct {
+	APIKey    string
+	APISecret string
+}
+
+// Config is a struct of List. You can use this struct to get any environment variable
+var Config List
+
+func init() {
+	cfg, err := ini.Load("../config.ini")
+	if err != nil {
+		log.Printf("Failed to load config.ini: %v", err)
+		os.Exit(1)
+	}
+
+	Config = List{
+		APIKey:    cfg.Section("bitflyer").Key("api_key").String(),
+		APISecret: cfg.Section("bitflyer").Key("api_secret").String(),
+	}
+}
