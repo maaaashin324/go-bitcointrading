fc8649fda7d18bc8c5a6ead597269ffc3f491c53
maaaashin324 <masataka.shintoku@gmail.com>
2020-07-11 11:00:58 +0900

    Added log_file to config.go

--- config/config.go
+++ config/config.go
@@ -11,6 +11,7 @@ import (
 type List struct {
 	APIKey    string
 	APISecret string
+	LOGFile   string
 }
 
 // Config is a struct of List. You can use this struct to get any environment variable
@@ -26,5 +27,6 @@ func init() {
 	Config = List{
 		APIKey:    cfg.Section("bitflyer").Key("api_key").String(),
 		APISecret: cfg.Section("bitflyer").Key("api_secret").String(),
+		LOGFile:   cfg.Section("gotrading").Key("log_file").MustString("gotrading.log"),
 	}
 }
