8c7b78dd34b5cbbdac28794f8c55ec39dc4baeab
maaaashin324 <masataka.shintoku@gmail.com>
2020-07-14 09:01:51 +0900

    Enabled to use api client in main.go

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"fmt"
+	"gotrading/bitflyer"
 	"gotrading/config"
 	"gotrading/utils"
 	"log"
+	"time"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LOGFile)
-	log.Println("test")
+	apiClient := bitflyer.New(config.Config.APIKey, config.Config.APISecret)
+	ticker, err := apiClient.GetTicker("BTC_JPY")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	fmt.Println(ticker)
+	fmt.Println(ticker.GetMidPrice())
+	fmt.Println(ticker.DateTime())
+	fmt.Println(ticker.TruncateDateTime(time.Minute))
 }
