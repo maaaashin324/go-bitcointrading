ed56aaf3984eb36762ffc4750793b449556dbbe7
maaaashin324 <masataka.shintoku@gmail.com>
2020-07-18 11:04:29 +0900

    Added candle.go

---
+++ app/models/candle.go
@@ -0,0 +1,89 @@
+package models
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"gotrading/bitflyer"
+)
+
+type Candle struct {
+	ProductCode string
+	Duration    time.Duration
+	Time        time.Time
+	Open        float64
+	Close       float64
+	High        float64
+	Low         float64
+	Volume      float64
+}
+
+func NewCandle(productCode string, duration time.Duration, timeDate time.Time, open, close, high, low, volume float64) *Candle {
+	return &Candle{
+		productCode,
+		duration,
+		timeDate,
+		open,
+		close,
+		high,
+		low,
+		volume,
+	}
+}
+
+func (candle *Candle) GetTableName() string {
+	return GetCandleTableName(candle.ProductCode, candle.Duration)
+}
+
+func (candle *Candle) Create() error {
+	cmd := fmt.Sprintf("INSERT INTO %s (time, open, close, high, low, volume) VALUES (?, ?, ?, ?, ?, ?)", candle.GetTableName())
+	_, err := DbConnection.Exec(cmd, candle.Time.Format(time.RFC3339), candle.Open, candle.Close, candle.High, candle.Low, candle.Volume)
+	if err != nil {
+		log.Printf("action=Create, err=%s", err.Error())
+		return err
+	}
+	return nil
+}
+
+func (candle *Candle) Save() error {
+	cmd := fmt.Sprintf("UPDATE %s SET open = ?, close = ?, high = ?, low = ?, volume = ? WHERE time = ?", candle.GetTableName())
+	_, err := DbConnection.Exec(cmd, candle.Open, candle.Close, candle.High, candle.Low, candle.Volume, candle.Time.Format(time.RFC3339))
+	if err != nil {
+		log.Printf("action=Save, err=%s", err.Error())
+		return err
+	}
+	return nil
+}
+
+func GetCandle(productCode string, duration time.Duration, dateTime time.Time) *Candle {
+	tableName := GetCandleTableName(productCode, duration)
+	cmd := fmt.Sprintf("SELECT time, open, close, high, low, volume FROM %s WHERE time = ?", tableName)
+	row := DbConnection.QueryRow(cmd, dateTime.Format(time.RFC3339))
+	var candle Candle
+	err := row.Scan(&candle.Time, &candle.Open, &candle.Close, &candle.High, &candle.Low, &candle.Volume)
+	if err != nil {
+		return nil
+	}
+	return NewCandle(productCode, duration, candle.Time, candle.Open, candle.Close, candle.High, candle.Low, candle.Volume)
+}
+
+func CreateCandleWithDuration(ticker bitflyer.Ticker, productCode string, duration time.Duration) bool {
+	currentCandle := GetCandle(productCode, duration, ticker.TruncateDateTime(duration))
+	price := ticker.GetMidPrice()
+	if currentCandle != nil {
+		candle := NewCandle(productCode, duration, ticker.TruncateDateTime(duration), price, price, price, price, ticker.Volume)
+		candle.Create()
+		return true
+	}
+
+	if currentCandle.High <= price {
+		currentCandle.High = price
+	} else if currentCandle.Low >= price {
+		currentCandle.Low = price
+	}
+	currentCandle.Volume += ticker.Volume
+	currentCandle.Close = price
+	currentCandle.Save()
+	return false
+}
