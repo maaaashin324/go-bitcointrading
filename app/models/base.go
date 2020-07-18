0507dfea90261b3a5fc9bbd8d7ead94e8043d3d2
maaaashin324 <masataka.shintoku@gmail.com>
2020-07-18 09:49:35 +0900

    Added base.go

---
+++ app/models/base.go
@@ -0,0 +1,57 @@
+package models
+
+import (
+	"database/sql"
+	"fmt"
+	"gotrading/config"
+	"log"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const (
+	tableNameSignalEvents = "signal_events"
+)
+
+var DbConnection *sql.DB
+
+func GetCandleTableName(productCode string, duration time.Duration) string {
+	return fmt.Sprintf("%s_%s", productCode, duration)
+}
+
+func init() {
+	var err error
+	DbConnection, err = sql.Open(config.Config.DbName, config.Config.SQLDriver)
+	if err != nil {
+		log.Fatalf("action=init in models, err=%s", err.Error())
+	}
+
+	cmd := fmt.Sprintf(`
+		CREATE TABLE IF NOT EXISTS %s (
+			time 					DATETIME PRIMARY KEY NOT NULL,
+			product_code	STRING,
+			side 					STRING,
+			price 				FLOAT,
+			size					FLOAT)`, tableNameSignalEvents)
+	_, err = DbConnection.Exec(cmd)
+	if err != nil {
+		log.Fatalf("action=init in models, err=%s", err.Error())
+	}
+
+	for _, duration := range config.Config.Durations {
+		tableName := GetCandleTableName(config.Config.ProductCode, duration)
+		cmd = fmt.Sprintf(`
+			CREATE TABLE IF NOT EXISTS %s (
+				time 			DATETIME PRIMARY KEY NOT NULL,
+				open			FLOAT,
+				close 		FLOAT,
+				high 			FLOAT,
+				low open	FLOAT,
+				volume		FLOAT)`, tableName)
+		_, err = DbConnection.Exec(cmd)
+		if err != nil {
+			log.Fatalf("action=init in models, err=%s", err.Error())
+		}
+	}
+}
