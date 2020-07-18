2c1ee2508754c0555001742d8e62a7f3f37fa76a
maaaashin324 <masataka.shintoku@gmail.com>
2020-07-18 11:15:39 +0900

    Added streamdata.go

---
+++ app/controllers/streamdata.go
@@ -0,0 +1,24 @@
+package controllers
+
+import (
+	"log"
+	
+	"gotrading/app/models"
+	"gotrading/bitflyer"
+	"gotrading/config"
+)
+
+func StreamIngestionData() {
+	var tickerChannel = make(chan bitflyer.Ticker)
+	apiClient := bitflyer.New(config.Config.APIKey, config.Config.APISecret)
+	go apiClient.GetRealtimeTicker(config.Config.ProductCode, tickerChannel)
+	for ticker := range tickerChannel {
+		log.Printf("action=StreamIngestionData, %v", ticker)
+		for _, duration := range config.Config.Durations {
+			isCreated := models.CreateCandleWithDuration(ticker, ticker.ProductCode, duration)
+			if isCreated && duration == config.Config.TradeDuration {
+				// TODO
+			}
+		}
+	}
+}
